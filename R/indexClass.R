b36dc3a5cbbc4a6830636f4c1fb1c16aad771361
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-01-10 21:40:12 +0000

    changed name of indexType to indexClass, following what it does;
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/pkg@20 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/indexClass.R
+++ R/indexClass.R
@@ -1,27 +1,27 @@
-`indexType` <-
+`indexClass` <-
 function(x,...) {
   if(!is.xts(x)) x <- as.xts(x)
   class(index(x))
 }
 
-`indexType<-` <-
+`indexClass<-` <-
 function(x,value) {
-  UseMethod('indexType<-')
+  UseMethod('indexClass<-')
 }
 
-`indexType<-.xts` <-
+`indexClass<-.xts` <-
 function(x,value) {
   if(!is.list(value)) value <- as.list(value)
   if(!value[[1]] %in% c('POSIXlt','POSIXct','Date','timeDate') )
-       stop(paste('illegal',sQuote('indexType'),'value:',as.character(value[[1]])))
+       stop(paste('illegal',sQuote('indexClass'),'value:',as.character(value[[1]])))
 
   if(value[[1]]=='timeDate') {
     stopifnot('package:fSeries' %in% search() | require('fSeries',quietly=TRUE))
-    index(x) <- do.call(paste('as',value[[1]],sep='.'),list(index(x),value))
+    index(x) <- do.call(paste('as',value[[1]],sep='.'),list(index(x)))
   } 
   else if(value[[1]] %in% c('POSIXct','POSIXlt')) {
-    index(x) <- do.call(paste('as',value[[1]],sep='.'),list(index(x),value))
-  } else index(x) <- do.call("as.Date",list(index(x),value))
+    index(x) <- do.call(paste('as',value[[1]],sep='.'),list(index(x)))
+  } else index(x) <- do.call("as.Date",list(index(x)))
   x
 }
 
