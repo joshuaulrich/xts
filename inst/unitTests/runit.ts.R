a1b76e35632098f6aced76f222c4b3bf347b068f
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-02-16 04:15:07 +0000

    new quarterly and monthly ts
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/pkg@79 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/unitTests/runit.ts.R
+++ inst/unitTests/runit.ts.R
@@ -29,3 +29,67 @@ test.ts_reclass_subset_as.xts_j1 <- function() {
 test.ts_reclass_subset_ts_j1 <- function() {
   checkIdentical(sample.ts1[,1],reclass(as.xts(sample.ts1[,1])))
 }
+
+# quarterly series
+sample.ts4 <- ts(sample_matrix,start=1960,frequency=4)
+sample.xts.ts4 <- as.xts(sample.ts4)
+
+
+test.convert_ts4_to_xts <- function() {
+  checkIdentical(sample.xts.ts4,as.xts(sample.ts4))
+}
+test.convert_ts4_to_xts_j1 <- function() {
+  checkIdentical(sample.xts.ts4[,1],as.xts(sample.ts4)[,1])
+}
+test.convert_ts4_to_xts_i1 <- function() {
+  checkIdentical(sample.xts.ts4[1,],as.xts(sample.ts4)[1,])
+}
+test.convert_ts4_to_xts_i1j1 <- function() {
+  checkIdentical(sample.xts.ts4[1,1],as.xts(sample.ts4)[1,1])
+}
+
+
+test.ts4_reclass <- function() {
+  checkIdentical(sample.ts4,reclass(as.xts(sample.ts4)))
+}
+test.ts4_reclass_subset_reclass_j1 <- function() {
+  checkIdentical(sample.ts4[,1],reclass(as.xts(sample.ts4))[,1])
+}
+test.ts4_reclass_subset_as.xts_j1 <- function() {
+  checkIdentical(sample.ts4[,1],reclass(as.xts(sample.ts4)[,1]))
+}
+test.ts4_reclass_subset_ts_j1 <- function() {
+  checkIdentical(sample.ts4[,1],reclass(as.xts(sample.ts4[,1])))
+}
+
+# monthly series
+sample.ts12 <- ts(sample_matrix,start=1990,frequency=12)
+sample.xts.ts12 <- as.xts(sample.ts12)
+
+
+test.convert_ts12_to_xts <- function() {
+  checkIdentical(sample.xts.ts12,as.xts(sample.ts12))
+}
+test.convert_ts12_to_xts_j1 <- function() {
+  checkIdentical(sample.xts.ts12[,1],as.xts(sample.ts12)[,1])
+}
+test.convert_ts12_to_xts_i1 <- function() {
+  checkIdentical(sample.xts.ts12[1,],as.xts(sample.ts12)[1,])
+}
+test.convert_ts12_to_xts_i1j1 <- function() {
+  checkIdentical(sample.xts.ts12[1,1],as.xts(sample.ts12)[1,1])
+}
+
+
+test.ts12_reclass <- function() {
+  checkIdentical(sample.ts12,reclass(as.xts(sample.ts12)))
+}
+test.ts12_reclass_subset_reclass_j1 <- function() {
+  checkIdentical(sample.ts12[,1],reclass(as.xts(sample.ts12))[,1])
+}
+test.ts12_reclass_subset_as.xts_j1 <- function() {
+  checkIdentical(sample.ts12[,1],reclass(as.xts(sample.ts12)[,1]))
+}
+test.ts12_reclass_subset_ts_j1 <- function() {
+  checkIdentical(sample.ts12[,1],reclass(as.xts(sample.ts12[,1])))
+}
