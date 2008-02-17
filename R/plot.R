0d8504b6311ad52ba34f03b2bf74875fb90d0a1a
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-02-17 04:24:53 +0000

    upped to v 0.0-10 added plot.xts method - work in progress set .Rbuildignore to ignore Unit and Vignette
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/pkg@83 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/plot.R
@@ -0,0 +1,36 @@
+`plot.xts` <- function(x, y=NULL, type='l', major.ticks='auto', minor.ticks=TRUE, ...) {
+  if(NCOL(x) > 1) warning('only the univariate series will be plotted')
+  if(is.null(y))
+    xycoords <- xy.coords(x)
+  time.scale <- periodicity(x)$scale
+  ticks <- function(ival, major.ticks, gt=2, lt=30) {
+    tick.opts <- c('years','months','weeks','days',
+                   'hours','minutes','seconds')
+    if(major.ticks %in% tick.opts) {
+      cl <- major.ticks[1]
+    } else {
+      is <- sapply(tick.opts,
+                   function(y) {
+                     length(endpoints(ival,y,1))-1
+                   })
+      cl <- names(is)[which(is > gt & is < lt)][1]
+    }
+    ep <- endpoints(ival,cl)
+    ep 
+  }
+  ep <- ticks(x,major.ticks)
+  x.labels <- format(index(x)[ep+1],'%n%b%n%Y')
+  if(time.scale== "weekly" | time.scale == "daily") 
+      x.labels <- format(index(x)[ep + 1], "%b %d%n%Y")
+  if(time.scale == "minute") 
+      x.labels <- format(index(x)[ep + 1], "%b %d%n%H:%M")
+  plot(1:NROW(x),xycoords$y, type=type, axes=FALSE, ann=FALSE, ...)
+  if(minor.ticks)
+    axis(1, at=1:NROW(x), labels=FALSE, col='#BBBBBB')
+  axis(1, at=ep, labels=x.labels, las=1, lwd=1, mgp=c(3,2,0)) 
+  axis(2)
+  box()
+  dots <- list(...)
+  if(!'main' %in% names(dots)) title(main=deparse(substitute(x)))
+  do.call('title',list(...))
+}
