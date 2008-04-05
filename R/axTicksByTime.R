16f82e7bc7553552665e5e43fc50a9e9453f9a92
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-04-05 13:54:38 +0000

    added conditional break to reduce calculation time by 50% commit 100!
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/pkg@100 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/axTicksByTime.R
+++ R/axTicksByTime.R
@@ -1,32 +1,30 @@
 axTicksByTime <- function(ival, ticks.on, k=1, gt = 2, lt = 30) {
     tick.opts <- c("years", "months", "weeks", "days", "hours", 
         "minutes", "seconds")
-    tick.k.opts <- c(10,5,2,1,
-                     7,1,
-                     1,
-                     1,
-                     4,2,1,
-                     30,15,1,
-                     1)
+    tick.k.opts <- c(10, 5, 2, 1, 7, 1, 1, 1, 4, 2, 1, 30, 15, 
+        1, 1)
     if (ticks.on %in% tick.opts) {
         cl <- ticks.on[1]
         ck <- k
-    }
+        ep <- endpoints(ival, cl, ck) 
+    }   
     else {
-        # create a string vector of "ticks.on k"
-        tick.opts <- paste(rep(tick.opts,c(4,2,1,1,3,3,1)),
-                           tick.k.opts)
-        is <- sapply(tick.opts, function(y) {
-              y <- strsplit(y,' ')[[1]]
-              length(endpoints(ival, y[1], as.numeric(y[2]))) - 1
-        })
+        tick.opts <- paste(rep(tick.opts, c(4, 2, 1, 1, 3, 3,  
+            1)), tick.k.opts)
+        is <- structure(rep(0,length(tick.opts)),.Names=tick.opts)
+        for(i in 1:length(tick.opts)) {
+          y <- strsplit(tick.opts[i],' ')[[1]]
+          ep <-endpoints(ival,y[1],as.numeric(y[2]))
+          is[i] <- length(ep) -1
+          if(is[i] > lt) 
+            break
+        }   
         nms <- rev(names(is)[which(is > gt & is < lt)])[1]
-        cl <- strsplit(nms,' ')[[1]][1]
-        ck <- as.numeric(strsplit(nms,' ')[[1]][2] )
-    }
-    if(is.null(cl)) {
-      ep <- NULL
-    } else ep <- endpoints(ival, cl, ck)
-    ep
+        cl <- strsplit(nms, " ")[[1]][1]
+        ck <- as.numeric(strsplit(nms, " ")[[1]][2])
+    }   
+    if (is.null(cl)) {
+        ep <- NULL
+    }   
+    ep  
 }
-
