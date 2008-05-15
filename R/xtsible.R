cc5c03f2ba384e5190422d8ec39c5364295fa0da
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-05-15 21:59:09 +0000

    new using.xts experimental function
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/pkg@120 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/xtsible.R
+++ R/xtsible.R
@@ -27,3 +27,11 @@ function(x, ..., error=TRUE)
     xx
   }
 }
+
+`using.xts` <- function(x) {
+  xx <- match.call()
+  xxObj <- eval(xx[[-1]][[2]])
+  inObj <- use.xts(xxObj, error=FALSE)
+  xx <- eval(match.call()[[-1]])
+  reclass(xx, inObj)
+}
