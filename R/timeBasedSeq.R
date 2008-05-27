301d8c7c717ba8e185b46c629b1145ef702117cc
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-05-27 16:18:18 +0000

    new timeBasedSeq function for ISO style range construction of valid xts index
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/pkg@131 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/timeBasedSeq.R
@@ -0,0 +1,54 @@
+`timeBasedSeq` <- 
+function(x, retclass=NULL, length.out=NULL) {
+  x <- gsub('::','/',x, perl=TRUE)  # replace all '::' range ops with '/'
+  x <- gsub('[-:]','',x, perl=TRUE) # strip all remaining '-' and ':' seps
+  x <- gsub('[ ]','',x, perl=TRUE) # strip all remaining white space
+  x <- unlist(strsplit(x,"/"))
+  from <- x[1]
+  to   <- x[2]
+  year  <- as.numeric(substr(from,1,4))
+  month <- as.numeric(substr(from,5,6))
+  day   <- as.numeric(substr(from,7,8))
+  hour  <- as.numeric(substr(from,9,10))
+  mins  <- as.numeric(substr(from,11,12))
+  secs  <- as.numeric(substr(from,13,14))
+
+  time.args.from <- as.list(unlist(sapply(c(year,month,day,hour,mins,secs),
+                               function(x) if(!is.na(x)) x)
+                        ))
+
+  from <- do.call('firstof',time.args.from) 
+
+  
+  year  <- as.numeric(substr(to,1,4))
+  month <- as.numeric(substr(to,5,6))
+  day   <- as.numeric(substr(to,7,8))
+  hour  <- as.numeric(substr(to,9,10))
+  mins  <- as.numeric(substr(to,11,12))
+  secs  <- as.numeric(substr(to,13,14))
+
+  time.args.to <- as.list(unlist(sapply(c(year,month,day,hour,mins,secs),
+                               function(x) if(!is.na(x)) x)
+                        ))
+
+  to <- do.call('lastof',time.args.to) 
+
+  max.resolution <- max(length(time.args.from), length(time.args.to))
+  resolution <- c('year','month','day','hour','mins','secs')[max.resolution]
+
+  convert.to <- 'Date'
+  if(max.resolution == 2) convert.to <- 'yearmon'
+  if(max.resolution >  3) convert.to <- 'POSIXct'
+
+  if(!is.null(retclass)) convert.to <- retclass
+
+  if(is.null(length.out)) {
+    SEQ <- seq(from,to,by=resolution)
+  } else {
+    SEQ <- seq(from, by=resolution, length.out=length.out)
+  }
+
+  do.call(paste('as',convert.to,sep='.'), list(SEQ))
+
+}
+
