a109f7e0dbea14556a56bbb8cc426fd2af91f754
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-07-23 04:31:50 +0000

    new timeBasedRange to return seconds since 1970-01-01 range for use in subsetting updated docs and NAMESPACE to match
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/pkg@147 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/timeBasedRange.R
@@ -0,0 +1,14 @@
+`createIndexRange` <-
+function(x, ...) {
+  # convert unquoted time range to
+  if (!is.character(x)) 
+    x <- deparse(match.call()$x)
+
+  # determine start and end points
+  tblist <- timeBasedSeq(x,NULL)
+
+  if(!is.null(tblist$length.out))
+    return(tblist$from)
+
+  c(as.numeric(tblist$from), as.numeric(tblist$to))
+}
