d2bb25f00511e094bc0e9a5048dbad70c163680a
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-07-30 04:35:49 +0000

    fixed its() constructor example in unitTests refactored [.xts for major speed improvements
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/dev/pkg@180 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/xts.methods.R
+++ R/xts.methods.R
@@ -1,7 +1,10 @@
 `[.xts` <-
 function(x, i, j, drop = TRUE, ...) 
 {
-startTime <- Sys.time()
+
+DEBUG <- TRUE
+AT <- FALSE # This is for internal testing at present will remove soon
+
     original.indexclass <- indexClass(x)
     original.class <- class(x)
     original.cols <- NCOL(x)
@@ -62,31 +65,33 @@ startTime <- Sys.time()
       }
       i <- i.tmp
     }
-   
+  
+START <- Sys.time() 
     # .subset is picky, 0's in the 'i' position cause failures
     zero.index <- binsearch(i, 0, NULL)
     if(!is.na(zero.index))
       i <- i[ -zero.index ]
 
     if (missing(j)) {
-      dn1 <- dimnames(x)[[1]]
-      x.index <- attr(x, 'index')[i]
-      if(is.null(DIM)) {
-        x <- .subset(x, i=i, drop=drop)
-      }
-      else x <- .subset(x, i=i, j=1:original.cols, drop=drop)
-
-#      if(is.null(DIM)) {
-#        x.tmp <- .subset(x, i=i, drop=drop)
-#      }
-#      else x.tmp <- .subset(x, i=i, j=1:original.cols, drop=drop)
-#      rm(x)
-#      x <- x.tmp
-#      rm(x.tmp)
-      attr(x, 'index') <- x.index
-      dim(x) <- c(length(i), original.cols)
-      dn <- list(dn1[i],original.names)
-      dimnames(x) <- dn
+        dn1 <- dimnames(x)[[1]]
+        x.index <- attr(x, 'index')[i]
+        cat("SUBSET index: ", Sys.time() - START,"\n")
+        if(is.null(DIM)) {
+          x.tmp <- .subset(x, i=i, drop=drop)
+        }
+        else x.tmp <- .subset(x, i=i, j=1:original.cols, drop=drop)
+        cat("SUBSET data: ", Sys.time() - START,"\n")
+        rm(x)
+        x <- x.tmp
+        rm(x.tmp)
+        if(AT) {  
+          attr(x, 'index') <- x.index
+          dim(x) <- c(length(i), original.cols)
+          dn <- list(dn1[i],original.names)
+          dimnames(x) <- dn
+        } 
+        cat("GLUE dimnames: ", Sys.time() - START,"\n")
+        dim2 <- original.cols
 
       if(!is.null(original.attr)) {
         for(ii in 1:length(original.attr)) {
@@ -94,10 +99,16 @@ startTime <- Sys.time()
           if(names(original.attr)[ii]=='.ROWNAMES') attr(x,'.ROWNAMES') <- original.attr[[ii]][i]
         }
       }
-      class(x) <- original.class
+
+      if(DEBUG) cat("GLUE all attributes back: ", Sys.time() - START,"\n")
+
+      #class(x) <- original.class
       if(!is.null(original.cols)) j <- 1:original.cols
     }
     else {
+
+        if(DEBUG) cat("Processing j: ", Sys.time() - START,"\n")
+
         j <- sapply(j, function(xx) {
                          if(is.character(xx)) {
                            which(xx==colnames(x))
@@ -105,31 +116,49 @@ startTime <- Sys.time()
                        })
         dn1 <- dimnames(x)[[1]]
         x.index <- attr(x, 'index')[i]
-#        x.tmp <- .subset(x, i=i, j=j, drop=drop)
-#        rm(x)
-#        x <- x.tmp
-        x <- .subset(x, i=i, j=j, drop=drop)
-        attr(x, 'index') <- x.index
-        dim(x) <- c(length(i), length(j))
-        dn <- list(dn1[i],original.names[j])
-        dimnames(x) <- dn
-
+        if(is.null(DIM)) {
+          x.tmp <- .subset(x, i=i, drop=drop)
+        }
+        else x.tmp <- .subset(x, i=i, j=j, drop=drop)
+        rm(x)
+        x <- x.tmp
+        rm(x.tmp)
+        if(AT) {
+          attr(x, 'index') <- x.index
+          dim(x) <- c(length(i), length(j))
+          dn <- list(dn1[i],original.names[j])
+          dimnames(x) <- dn
+        }
+        dim2 <- length(j)
         if(!is.null(original.attr)) {
           for(ii in 1:length(original.attr)) {
             attr(x,names(original.attr)[ii]) <- original.attr[[ii]]
           }
         }
-        # handle future xts extensions without [. method rewrite
-        class(x) <- original.class
+        #class(x) <- original.class
     }
-    if (!is.null(dim(x))) 
-        colnames(x) <- original.names[j]
+if(AT) {
+    class(x) <- original.class
+if(DEBUG) cat("Set class: ", Sys.time() - START,"\n")
+    #if (!is.null(dim(x))) 
+    #    colnames(x) <- original.names[j]
+if(DEBUG) cat("Set colnames: ", Sys.time() - START,"\n")
 
     indexClass(x) <- original.indexclass
+if(DEBUG) cat("Set indexClass: ", Sys.time() - START,"\n")
 
     Sys.setenv(TZ=sys.TZ)
+if(DEBUG) cat("Set Sys.setenv: ", Sys.time() - START,"\n")
 
+    #attributes(x) <- list(.CLASS=original.CLASS, .indexCLASS=original.indexclass)
     CLASS(x) <- original.CLASS
+if(DEBUG) cat("Set CLASS: ", Sys.time() - START,"\n")
+} else {
+    attributes(x) <- c(attributes(x),list(index=x.index, dim=c(length(i), dim2),
+                       dimnames=list(dn1[i],original.names[j]),class=original.class,
+                       .indexCLASS=original.indexclass, .CLASS=original.CLASS))
+if(DEBUG) cat("Set colnames, indexClass and CLASS all at once: ", Sys.time()-START,"\n")
+}
     x
 }
 
--- inst/unitTests/runit.its.R
+++ inst/unitTests/runit.its.R
@@ -1,8 +1,12 @@
 library(its)
 data(sample_matrix)
+sys.TZ <- Sys.getenv('TZ')
+Sys.setenv(TZ='GMT')
 
-sample.its <- its(sample_matrix)
+sample.its <- its(sample_matrix,as.POSIXct(rownames(sample_matrix)))
 sample.xts <- as.xts(sample.its)
+
+
 
 test.convert_its_to_xts <- function() {
   checkIdentical(sample.xts,as.xts(sample.its))
@@ -17,14 +21,20 @@ test.convert_its_to_xts_i1j1 <- function() {
   checkIdentical(sample.xts[1,1],as.xts(sample.its)[1,1])
 }
 test.its_reclass <- function() {
+#  DEACTIVATED()
   checkIdentical(sample.its,reclass(as.xts(sample.its)))
 }
 test.its_reclass_subset_reclass_j1 <- function() {
+#  DEACTIVATED()
   checkIdentical(sample.its[,1],reclass(as.xts(sample.its))[,1])
 }
 test.its_reclass_subset_as.xts_j1 <- function() {
+#  DEACTIVATED()
   checkIdentical(sample.its[,1],reclass(as.xts(sample.its)[,1]))
 }
 test.its_reclass_subset_its_j1 <- function() {
+#  DEACTIVATED()
   checkIdentical(sample.its[,1],reclass(as.xts(sample.its[,1])))
 }
+
+Sys.setenv(TZ=sys.TZ)
