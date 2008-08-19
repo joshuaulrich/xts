be222df3ce33d6eea2338a196f20cccb97250fc3
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-08-19 14:44:52 +0000

    removed old file
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/dev/pkg@204 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/is.ordered.R
+++
@@ -1,7 +0,0 @@
-`isOrdered` <- function(x, increasing=TRUE, strictly=TRUE) {
-  # x must be of type double or integer.  Checked in the C code.
-  .Call('do_is_ordered', 
-        x = x,
-        increasing = as.logical(increasing),
-        strictly   = as.logical(strictly), PACKAGE='xts')
-}
