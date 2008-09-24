3faed27be782369d02d0fd6f5e197f138a20e610
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-09-24 16:49:58 +0000

    merge.xts0 code for Ops.xts streamlined
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/dev/pkg@239 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/merge.R
+++ R/merge.R
@@ -97,15 +97,6 @@
 
 `merge.xts0` <- function(x,y,...,all=TRUE, fill=NA, suffixes=NULL, join="outer", retclass='xts') {
   # this is for use withing Ops.xts, as it handles colnames differently
-  # merge is currently optimized for the 2 case
-  # scenario, but will handle k-merges via the overflow
-  # ... arg.
-  
-  # still need an implementation with do_merge_xts
-  # that can handle:
-  #  suffixes=
-  #
-  # to match the behavior of zoo
   if( missing(y) ) 
     return(x)
 
@@ -142,53 +133,14 @@
   if( length(all) == 1 )
     all <- rep(all, length.out=2)
 
-  dots <- list(...)
+  x <- .Call('do_merge_xts', x, y, all, fill[1], setclass, c(colnames(x), colnames(y)), PACKAGE="xts")
 
-  xyNames <- as.character(c(list(x=deparse(substitute(x)),y=deparse(substitute(y))),
-               as.character(as.list(match.call(expand.dots=FALSE)$...))))
-
-#  make.colnames <- function(x, xname, y, yname, suffix=NULL) {
-#    colnamesX <- colnames(x)
-#    if(is.null(colnamesX)) {
-#      colnamesX <- c(xname,paste(xname,1:(NCOL(x)-1),sep='.'))[1:NCOL(x)]
-#    }
-#    colnamesY <- colnames(y)
-#    if(is.null(colnamesY)) {
-#      colnamesY <- c(yname,paste(yname,1:(NCOL(y)-1),sep='.'))[1:NCOL(y)]
-#    }
-#    return(make.unique(c(colnamesX, colnamesY)))
-#  }
-#
-#  if(length(dots) > 0) {
-#
-#    x <- .Call('do_merge_xts', x, y, all, fill[1], setclass, 
-#               make.colnames(x, deparse(substitute(x)), y, deparse(substitute(y))), PACKAGE="xts")
-#    for(i in 1:length(dots)) {
-#      if( !is.xts(dots[[i]]) ) {
-#        dots[[i]] <- try.xts(dots[[i]], error=FALSE)
-#        if( !is.xts(dots[[i]]) && NROW(dots[[i]]) == NROW(x) ) {
-#          dots[[i]] <- structure(dots[[i]], index=.index(x))
-#        } else
-#        if( !is.xts(dots[[i]]) && NROW(dots[[i]])==1 && NCOL(dots[[i]])==1) {
-#          dots[[i]] <- structure(rep(dots[[i]], length.out=NROW(x)), index=.index(x)) 
-#        } else stop("can not convert 'y' to suitable class for merge")
-#      }
-#      x <- .Call('do_merge_xts', x, dots[[i]], all, fill[1], setclass, 
-#                  ,PACKAGE="xts")
-#    }
-#    return(x)
-#  } else {
+  if(is.null(retclass)) {
     ncx <- 1:NCOL(x)
-    ncy <- 1:NCOL(y)
-    x <- .Call('do_merge_xts', x, y, all, fill[1], setclass, c(colnames(x), colnames(y)), PACKAGE="xts")
-              # make.colnames(x,deparse(substitute(x)),y, deparse(substitute(y))), PACKAGE="xts")
-              # make.unique(c(colnamesX, colnamesY)), PACKAGE="xts")
-    if(is.null(retclass)) {
-      # needed for original Ops.xts(e1,e2), now for zoo compat
-      assign(xyNames$x, x[,ncx], parent.frame())
-      assign(xyNames$y, x[,-ncx],parent.frame())
-      invisible(return(NULL))
-    } else
-    return(x)
-#  }
+    # needed for original Ops.xts(e1,e2), now for zoo compat
+    assign(xyNames$x, x[,ncx], parent.frame())
+    assign(xyNames$y, x[,-ncx],parent.frame())
+    invisible(return(NULL))
+  } else
+  return(x)
 }
