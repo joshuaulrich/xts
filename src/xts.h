0cc1118f2d4bcbc38648c1d6cf2187b0aa90b0ed
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-10-01 03:22:19 +0000

    more MACROS for major xts attribute manipulation
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/dev/pkg@252 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/xts.h
+++ src/xts.h
@@ -19,23 +19,39 @@ as the full xts software, GPL3.
 /*
 INTERNAL SYMBOLS
 */
-#define  xts_IndexSymbol        install("index")
-#define  xts_ClassSymbol        install(".CLASS")
-#define  xts_IndexFormatSymbol  install(".indexFORMAT")
-#define  xts_IndexClassSymbol   install(".indexCLASS")
+#define  xts_IndexSymbol                install("index")
+#define  xts_ClassSymbol                install(".CLASS")
+#define  xts_IndexFormatSymbol          install(".indexFORMAT")
+#define  xts_IndexClassSymbol           install(".indexCLASS")
 
 
 /*
 DATA TOOLS
 */
-#define  xts_ATTRIB(x)          coerceVector(do_xtsAttributes(x),LISTSXP)
-#define  xts_COREATTRIB(x)          coerceVector(do_xtsCoreAttributes(x),LISTSXP)
+#define  xts_ATTRIB(x)                  coerceVector(do_xtsAttributes(x),LISTSXP)
+#define  xts_COREATTRIB(x)              coerceVector(do_xtsCoreAttributes(x),LISTSXP)
+
+// attr(x, 'index') or .index(x)
+#define  GET_xtsIndex(x)                getAttrib(x, xts_IndexSymbol)
+#define  SET_xtsIndex(x,value)          setAttrib(x, xts_IndexSymbol, value)
+
+// attr(x, '.indexCLASS') or indexClass(x)
+#define  GET_xtsIndexClass(x)           getAttrib(x, xts_IndexClassSymbol)
+#define  SET_xtsIndexvalue(x,value)     setAttrib(x, xts_IndexvalueSymbol, value)
+
+// attr(x, '.indexFORMAT') or indexFormat(x)
+#define  GET_xtsIndexFormat(x)          getAttrib(x, xts_IndexFormatSymbol)
+#define  SET_xtsIndexFormat(x,value)    setAttrib(x, xts_IndexFormatSymbol, value)
+
+// attr(x, '.CLASS') or CLASS(x)
+#define  GET_xtsCLASS(x)                getAttrib(x, xts_ClassSymbol)
+#define  SET_xtsCLASS(x,value)          setAttrib(x, xts_ClassSymbol, value)
 
 /*
 FUNCTIONS
 */
 SEXP do_xtsAttributes(SEXP x);              // xtsAttributes
-SEXP do_xtsCoreAttributes(SEXP x);              // xtsCoreAttributes xts-specific attributes
+SEXP do_xtsCoreAttributes(SEXP x);          // xtsCoreAttributes xts-specific attributes
 
 void copy_xtsAttributes(SEXP x, SEXP y);    
 void copy_xtsCoreAttributes(SEXP x, SEXP y);    
