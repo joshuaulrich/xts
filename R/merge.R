5ae0e536f74b0b1ab2005d1b4389fb0644eeef2d
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-10-15 17:06:59 +0000

    commented out merge.xts0
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/dev/pkg@267 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/merge.R
+++ R/merge.R
@@ -109,52 +109,52 @@
   }
 }
 
-`merge.xts0` <- function(x,y,...,all=TRUE, fill=NA, suffixes=NULL, join="outer", retclass='xts') {
-  # this is for use withing Ops.xts, as it handles colnames differently
-  if( missing(y) ) 
-    return(x)
-
-  if(is.logical(retclass) && !retclass) {
-    setclass <- FALSE
-  } else setclass <- TRUE
-
-  if( !is.xts(y) ) {
-    y <- try.xts(y, error=FALSE)
-    if( !is.xts(y) && NROW(y) == NROW(x) ) {
-      y <- structure(y, index=.index(x))
-    } else
-    if( !is.xts(y) && NROW(y)==1 && NCOL(y)==1) {
-      y <- structure(rep(y, length.out=NROW(x)), index=.index(x))
-    } else stop("cannot convert 'y' to suitable class for merge")
-  }
-
-  if( !missing(join) ) {
-    # join logic applied to index:
-    # inspired by: http://blogs.msdn.com/craigfr/archive/2006/08/03/687584.aspx
-    #
-    #  (full) outer - all cases, equivelant to all=c(TRUE,TRUE)
-    #         left  - all x,    &&  y's that match x
-    #         right - all  ,y   &&  x's that match x
-    #         inner - only x and y where index(x)==index(y)
-    all <- switch(pmatch(join,c("outer","left","right","inner")),
-                    c(TRUE,  TRUE ), #  outer
-                    c(TRUE,  FALSE), #  left
-                    c(FALSE, TRUE ), #  right
-                    c(FALSE, FALSE)  #  inner
-                 )
-  }
-
-  if( length(all) == 1 )
-    all <- rep(all, length.out=2)
-
-  x <- .Call('do_merge_xts', x, y, all, fill[1], setclass, c(colnames(x), colnames(y)), PACKAGE="xts")
-
-  if(is.null(retclass)) {
-    ncx <- 1:NCOL(x)
-    # needed for original Ops.xts(e1,e2), now for zoo compat
-    assign(xyNames[1], x[,ncx], parent.frame())
-    assign(xyNames[2], x[,-ncx],parent.frame())
-    invisible(return(NULL))
-  } else
-  return(x)
-}
+#`merge.xts0` <- function(x,y,...,all=TRUE, fill=NA, suffixes=NULL, join="outer", retclass='xts') {
+#  # this is for use withing Ops.xts, as it handles colnames differently
+#  if( missing(y) ) 
+#    return(x)
+#
+#  if(is.logical(retclass) && !retclass) {
+#    setclass <- FALSE
+#  } else setclass <- TRUE
+#
+#  if( !is.xts(y) ) {
+#    y <- try.xts(y, error=FALSE)
+#    if( !is.xts(y) && NROW(y) == NROW(x) ) {
+#      y <- structure(y, index=.index(x))
+#    } else
+#    if( !is.xts(y) && NROW(y)==1 && NCOL(y)==1) {
+#      y <- structure(rep(y, length.out=NROW(x)), index=.index(x))
+#    } else stop("cannot convert 'y' to suitable class for merge")
+#  }
+#
+#  if( !missing(join) ) {
+#    # join logic applied to index:
+#    # inspired by: http://blogs.msdn.com/craigfr/archive/2006/08/03/687584.aspx
+#    #
+#    #  (full) outer - all cases, equivelant to all=c(TRUE,TRUE)
+#    #         left  - all x,    &&  y's that match x
+#    #         right - all  ,y   &&  x's that match x
+#    #         inner - only x and y where index(x)==index(y)
+#    all <- switch(pmatch(join,c("outer","left","right","inner")),
+#                    c(TRUE,  TRUE ), #  outer
+#                    c(TRUE,  FALSE), #  left
+#                    c(FALSE, TRUE ), #  right
+#                    c(FALSE, FALSE)  #  inner
+#                 )
+#  }
+#
+#  if( length(all) == 1 )
+#    all <- rep(all, length.out=2)
+#
+#  x <- .Call('do_merge_xts', x, y, all, fill[1], setclass, c(colnames(x), colnames(y)), PACKAGE="xts")
+#
+#  if(is.null(retclass)) {
+#    ncx <- 1:NCOL(x)
+#    # needed for original Ops.xts(e1,e2), now for zoo compat
+#    assign(xyNames[1], x[,ncx], parent.frame())
+#    assign(xyNames[2], x[,-ncx],parent.frame())
+#    invisible(return(NULL))
+#  } else
+#  return(x)
+#}
