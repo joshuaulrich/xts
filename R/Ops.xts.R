2519b5de64e09c3ad0cbe158304380bc08bb10ca
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-10-15 17:00:42 +0000

    using merge.xts in place of merge.xts0 in Ops.xts
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/dev/pkg@266 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/Ops.xts.R
+++ R/Ops.xts.R
@@ -4,72 +4,20 @@ function(e1, e2)
   e <- if (missing(e2)) {
       .Class <- "matrix"
       NextMethod(.Generic)
-#      switch(.Generic,
-#        "!"  = !unclass(e1),
-#        "-"  = -unclass(e1),
-#        "+"  = +unclass(e1))
   }
   else if (any(nchar(.Method) == 0)) {
       .Class <- "matrix"
       NextMethod(.Generic)
-#      switch(.Generic,
-#        "+"   = unclass(e1) + unclass(e2),
-#        "-"   = unclass(e1) - unclass(e2),
-#        "*"   = unclass(e1) * unclass(e2),
-#        "/"   = unclass(e1) / unclass(e2),
-#        "^"   = unclass(e1) ^ unclass(e2),
-#        "%%"  = unclass(e1) %% unclass(e2),
-#        "%/%" = unclass(e1) %/% unclass(e2),
-#        "!="  = unclass(e1) == unclass(e2),
-#        ">"   = unclass(e1) > unclass(e2),
-#        "<"   = unclass(e1) < unclass(e2),
-#        ">="  = unclass(e1) >= unclass(e2),
-#        "<="  = unclass(e1) <= unclass(e2),
-#        "=="  = unclass(e1) == unclass(e2))
   }
   else {
     if( NROW(e1)==NROW(e2) && identical(.index(e1),.index(e2)) ) {
     .Class <- "matrix"
     NextMethod(.Generic)
-#      switch(.Generic,
-#        "+"   = unclass(e1) + unclass(e2),
-#        "-"   = unclass(e1) - unclass(e2),
-#        "*"   = unclass(e1) * unclass(e2),
-#        "/"   = unclass(e1) / unclass(e2),
-#        "^"   = unclass(e1) ^ unclass(e2),
-#        "%%"  = unclass(e1) %% unclass(e2),
-#        "%/%" = unclass(e1) %/% unclass(e2),
-#        "!="  = unclass(e1) == unclass(e2),
-#        ">"   = unclass(e1) > unclass(e2),
-#        "<"   = unclass(e1) < unclass(e2),
-#        ">="  = unclass(e1) >= unclass(e2),
-#        "<="  = unclass(e1) <= unclass(e2),
-#        "=="  = unclass(e1) == unclass(e2))
     } else {
-      #nc1 <- 1:NCOL(e1)
-      #E <- merge.xts0(e1, e2, all=FALSE, retclass=FALSE)
-      #if(is.null(E)) 
-      #  return(E)
-      #e1 <- .subset.xts(E, j= nc1) #[,  nc1] 
-      #e2 <- .subset.xts(E, j=-nc1) #E[, -nc1] 
-      e1 <- merge.xts0(e1, .xts(,.index(e2)), all=FALSE, retclass=FALSE)
-      e2 <- merge.xts0(e2, .xts(,.index(e1)), all=FALSE, retclass=FALSE)
+      e1 <- merge.xts(e1, .xts(,.index(e2)), all=FALSE, retclass=FALSE)
+      e2 <- merge.xts(e2, .xts(,.index(e1)), all=FALSE, retclass=FALSE)
       .Class <- "matrix"
       NextMethod(.Generic)
-#      switch(.Generic,
-#        "+"   = e1 + e2,
-#        "-"   = e1 - e2,
-#        "*"   = e1 * e2,
-#        "/"   = e1 / e2,
-#        "^"   = e1 ^ e2,
-#        "%%"  = e1 %% e2,
-#        "%/%"  = e1 %/% e2,
-#        "!="  = e1 != e2,
-#        ">"   = e1 > e2,
-#        "<"   = e1 < e2,
-#        ">="  = e1 >= e2,
-#        "<="  = e1 <= e2,
-#        "=="  = e1 == e2)
     }
   }
   if(.Generic %in% c("+","-","*","/","^","%%","%/%")) {
