4d002eece85014f296aa9f1224a326d6ac739193
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-11-01 04:41:57 +0000

    check for scalar vs. zero-width objects in column naming
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/dev/pkg@314 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/merge.R
+++ R/merge.R
@@ -21,9 +21,8 @@ merge.xts <- function(...,
   .env <- rev(sys.frames())[[1]]
   .times <- unlist(lapply(dots, function(x) {
                          nc <- ncol(eval(x, envir=.env))
-                         if(is.null(nc) && (is.numeric(eval(x, envir=.env)) 
-                                            || is.integer(eval(x, envir=.env))
-                                            || is.logical(eval(x, envir=.env)))) nc <- 1
+                         if(is.null(nc) && length(eval(x, envir=.evv)) > 0)
+                            nc <- 1
                          ifelse(is.null(nc), 0, nc)
                          }))
   cnames <- make.names(rep(syms, .times), unique=TRUE)
