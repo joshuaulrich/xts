b8e9c31fbf079d953c7927c2e52315d91b7d9e0f
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2008-12-10 16:59:38 +0000

    fixed bug in cum**** where ncol > 1, and now correctly returns xts object
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/dev/pkg@348 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/Math.xts.R
+++ R/Math.xts.R
@@ -1,27 +1,31 @@
 cumsum.xts <- function(x)
 {
-  if( NCOL(x) == 1 )
-    return(cumsum(coredata(x)))
-  apply(x, 2, function(y) cumsum(coredata(x)))
+  if( NCOL(x) == 1 ) {
+    x[] <- cumsum(coredata(x))
+  } else x[] <- apply(coredata(x), 2, function(y) cumsum(y))
+  x
 }
 
 cumprod.xts <- function(x)
 {
-  if( NCOL(x) == 1 )
-    return(cumprod(coredata(x)))
-  apply(x, 2, function(y) cumprod(coredata(x)))
+  if( NCOL(x) == 1 ) {
+    x[] <- cumprod(coredata(x))
+  } else x[] <- apply(coredata(x), 2, function(y) cumprod(y))
+  x
 }
 
 cummin.xts <- function(x)
 {
-  if( NCOL(x) == 1 )
-    return(cummin(coredata(x)))
-  apply(x, 2, function(y) cummin(coredata(x)))
+  if( NCOL(x) == 1 ) {
+    x[] <- cummin(coredata(x))
+  } else x[] <- apply(coredata(x), 2, function(y) cummin(y))
+  x
 }
 
 cummax.xts <- function(x)
 {
-  if( NCOL(x) == 1 )
-    return(cummax(coredata(x)))
-  apply(x, 2, function(y) cummax(coredata(x)))
+  if( NCOL(x) == 1 ) {
+    x[] <- cummax(coredata(x))
+  } else x[] <- apply(coredata(x), 2, function(y) cummax(y))
+  x
 }
