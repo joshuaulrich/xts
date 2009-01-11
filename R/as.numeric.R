2346017e28431d657738e87b5a234af2988c622d
Joshua M. Ulrich <josh.m.ulrich@gmail.com>
2009-01-11 04:35:26 +0000

    - Added and registered as.xts and re.xxx methods for the following   classes: numeric, double, integer, complex, logical
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/dev/pkg@374 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/as.numeric.R
+++ R/as.numeric.R
@@ -26,11 +26,57 @@ as.numeric.xts <- function(x, drop=TRUE, ...)
   .xts(matrix(as.numeric(unclass(x)),nc=NCOL(x)), .index(x))
 }
 
+as.xts.numeric <-
+function(x,order.by,dateFormat="POSIXct",frequency=NULL,...) {
+  # jmu
+  if(missing(order.by)) {
+    if(is.null(names(x)))
+      stop("order.by must be either 'names()' or otherwise specified")
+    else
+      # added '...' args to allow for tz specification
+      order.by <- do.call(paste('as',dateFormat,sep='.'),list(names(x)))
+  }
+  xx <- xts(x, order.by=order.by, frequency=frequency,
+            .CLASS='numeric', ...)
+  return(xx)
+}
+
+re.numeric <-
+function(x,...) {
+  # jmu
+  y <- as.numeric(x,...)
+  names(y) <- index(x)
+  return(y)
+}
+
 as.integer.xts <- function(x, drop=TRUE, ...)
 {
   if(drop)
     return(as.integer(unclass(x)))
   .xts(matrix(as.integer(unclass(x)),nc=NCOL(x)), .index(x))
+}
+
+as.xts.integer <-
+function(x,order.by,dateFormat="POSIXct",frequency=NULL,...) {
+  # jmu
+  if(missing(order.by)) {
+    if(is.null(names(x)))
+      stop("order.by must be either 'names()' or otherwise specified")
+    else
+      # added '...' args to allow for tz specification
+      order.by <- do.call(paste('as',dateFormat,sep='.'),list(names(x)))
+  }
+  xx <- xts(x, order.by=order.by, frequency=frequency,
+            .CLASS='integer', ...)
+  return(xx)
+}
+
+re.integer <-
+function(x,...) {
+  # jmu
+  y <- as.integer(x,...)
+  names(y) <- index(x)
+  return(y)
 }
 
 as.double.xts <- function(x, drop=TRUE, ...)
@@ -40,11 +86,57 @@ as.double.xts <- function(x, drop=TRUE, ...)
   .xts(matrix(as.double(unclass(x)),nc=NCOL(x)), .index(x))
 }
 
+as.xts.double <-
+function(x,order.by,dateFormat="POSIXct",frequency=NULL,...) {
+  # jmu
+  if(missing(order.by)) {
+    if(is.null(names(x)))
+      stop("order.by must be either 'names()' or otherwise specified")
+    else
+      # added '...' args to allow for tz specification
+      order.by <- do.call(paste('as',dateFormat,sep='.'),list(names(x)))
+  }
+  xx <- xts(x, order.by=order.by, frequency=frequency,
+            .CLASS='double', ...)
+  return(xx)
+}
+
+re.double <-
+function(x,...) {
+  # jmu
+  y <- as.double(x,...)
+  names(y) <- index(x)
+  return(y)
+}
+
 as.complex.xts <- function(x, drop=TRUE, ...)
 {
   if(drop)
     return(as.complex(unclass(x)))
   .xts(matrix(as.complex(unclass(x)),nc=NCOL(x)), .index(x))
+}
+
+as.xts.complex <-
+function(x,order.by,dateFormat="POSIXct",frequency=NULL,...) {
+  # jmu
+  if(missing(order.by)) {
+    if(is.null(names(x)))
+      stop("order.by must be either 'names()' or otherwise specified")
+    else
+      # added '...' args to allow for tz specification
+      order.by <- do.call(paste('as',dateFormat,sep='.'),list(names(x)))
+  }
+  xx <- xts(x, order.by=order.by, frequency=frequency,
+            .CLASS='complex', ...)
+  return(xx)
+}
+
+re.complex <-
+function(x,...) {
+  # jmu
+  y <- as.complex(x,...)
+  names(y) <- index(x)
+  return(y)
 }
 
 as.logical.xts <- function(x, drop=TRUE, ...)
@@ -53,3 +145,27 @@ as.logical.xts <- function(x, drop=TRUE, ...)
     return(as.logical(unclass(x)))
   .xts(matrix(as.logical(unclass(x)),nc=NCOL(x)), .index(x))
 }
+
+as.xts.logical <-
+function(x,order.by,dateFormat="POSIXct",frequency=NULL,...) {
+  # jmu
+  if(missing(order.by)) {
+    if(is.null(names(x)))
+      stop("order.by must be either 'names()' or otherwise specified")
+    else
+      # added '...' args to allow for tz specification
+      order.by <- do.call(paste('as',dateFormat,sep='.'),list(names(x)))
+  }
+  xx <- xts(x, order.by=order.by, frequency=frequency,
+            .CLASS='logical', ...)
+  return(xx)
+}
+
+re.logical <-
+function(x,...) {
+  # jmu
+  y <- as.logical(x,...)
+  names(y) <- index(x)
+  return(y)
+}
+
