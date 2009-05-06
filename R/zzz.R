a69a35fefddcaf42ed18dd017a4b1498bfdbb065
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2009-05-06 14:06:51 +0000

    new version 0.6-5
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/pkg@411 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,11 @@
 .onLoad <- function(lib, pkg)
 {
   if(Sys.getenv("TZ") == "") {
-    message("xts now requires a valid TZ variable to be set")
-    message(" no TZ var is set, setting to TZ=GMT")
+    cat("xts now requires a valid TZ variable to be set\n")
+    cat(" no TZ var is set, setting to TZ=GMT")
     Sys.setenv(TZ="GMT")
   } else {
-    message("xts now requires a valid TZ variable to be set")
-    message(" your current TZ:", paste(Sys.getenv("TZ")))
+    cat("xts now requires a valid TZ variable to be set\n")
+    cat(" your current TZ:",paste(Sys.getenv("TZ")))
   }
 }
