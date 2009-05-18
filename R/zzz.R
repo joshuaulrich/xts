80c362303cca87e7bebc346d10bcc3ceba0195af
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2009-05-18 15:24:59 +0000

    removed as.zoo.xts (in zoo package -- thanks Gabor for pointing it out) fixed startup message missing line-break
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/pkg@412 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,10 @@
 {
   if(Sys.getenv("TZ") == "") {
     cat("xts now requires a valid TZ variable to be set\n")
-    cat(" no TZ var is set, setting to TZ=GMT")
+    cat(" no TZ var is set, setting to TZ=GMT\n")
     Sys.setenv(TZ="GMT")
   } else {
     cat("xts now requires a valid TZ variable to be set\n")
-    cat(" your current TZ:",paste(Sys.getenv("TZ")))
+    cat(" your current TZ:",paste(Sys.getenv("TZ")),"\n")
   }
 }
