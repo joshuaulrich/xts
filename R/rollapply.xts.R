fdfd3148b2a3af4c7cd19792b903204965a33cd2
Joshua M. Ulrich <josh.m.ulrich@gmail.com>
2009-10-07 02:04:05 +0000

    - Updates to rollapply.xts to run FUN on xts objects
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/pkg@450 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/rollapply.xts.R
+++ R/rollapply.xts.R
@@ -1,6 +1,16 @@
-rollapply.xts <- function(data, width, FUN, ..., by=1, ascending=TRUE, by.column=TRUE,
-  na.pad=FALSE, align=c("center","left","right"), cumulative=FALSE) {
+rollapply.xts <- function(data, width, FUN, ..., by=1, ascending=TRUE,
+  by.column=TRUE, na.pad=FALSE, align=c("center","left","right")) {
 
+  if(!ascending) {
+    warning("ignoring 'ascending=FALSE'")
+    ascending <- TRUE
+  }
+  if(by != 1) {
+    warning(paste("ignoring 'by=",by,"'",sep=""))
+    by <- 1
+  }
+  if(missing(align)) align <- "right"
+  
   # Code taken/adapted from rollapply.zoo from the 'zoo' package
 
   embedi <- function(n, k, by = 1, ascending = FALSE) {
@@ -23,9 +33,8 @@ rollapply.xts <- function(data, width, FUN, ..., by=1, ascending=TRUE, by.column
   #    max = return(rollmax(data, width, na.pad = na.pad, align = align)),
   #    median = return(rollmedian(data, width, na.pad = na.pad, align = align)))
 
-  ## evaluate FUN only on coredata(data)
-  cdata <- coredata(data)
-  nr <- NROW(cdata)
+  nr <- NROW(data)
+  nc <- NCOL(data)
   width <- as.integer(width)[1]
   stopifnot( width > 0, width <= nr )
 
@@ -34,31 +43,44 @@ rollapply.xts <- function(data, width, FUN, ..., by=1, ascending=TRUE, by.column
   n1 <- switch(align,    
     "left" = { width - 1},
     "center" = { floor(width/2) },
-    "right" = { 0 })    
-  tt <- index(data)[seq((width-n1), (nr-n1), by)]
+    "right" = { 0 })
+  idx <- index(data)
+  #tt <- index(data)[seq((width-n1), (nr-n1), by)]
+  tt <- idx[seq((width-n1), (nr-n1), 1)]
+
+  ## evaluate FUN only on coredata(data)
+  #data <- coredata(data)
 
   FUN <- match.fun(FUN)
-  e <- embedi(nr, width, by, ascending)
-  
-  if( is.null(dim(cdata)) ) {
-    xx <- sapply(1:nrow(e), function(i) FUN(cdata[e[i,]], ...))
-    if( !is.null(dim(xx)) ) xx <- t(xx)
+
+  ind <- as.matrix(width:nr)
+  #e <- embedi(nr, width, by, ascending)
+
+  if( nc==1 ) {
+    #xx <- apply(e, 1, function(i) FUN(data[i,],...))
+    #xx <- sapply(1:NROW(e), function(i) FUN(data[e[i,],],...))
+    xx <- sapply(ind, function(i) FUN(data[(i-width+1):i,],...))
     res <- xts(xx, tt, if (by == 1) attr(data, "frequency"))
   } else if( by.column ) {
-    # e <- embedi(nr, width, by, ascending)
-    res <- xts( sapply( 1:ncol(cdata), function(i)
-	                apply( e, 1, function(st) FUN(cdata[st,i], ...) ) ),
+    res <- xts( sapply( 1:NCOL(data), function(j)
+                #apply(e, 1, function(i) FUN(data[i,j],...)) ),
+                apply(ind, 1, function(i) FUN(data[(i-width+1):i,j],...)) ),
 		        tt, if (by == 1) attr(data, "frequency") )
   } else {
-    rval <- apply(embedi(nr, width, by, ascending), 1, function(st) FUN(cdata[st,], ...))
-    if(!is.null(dim(rval))) rval <- t(rval)
-    res <- xts(rval, tt, if (by == 1) attr(data, "frequency"))
-  }	   
-  if( na.pad ) {
-    res <- merge(res, xts(,index(data), attr(data, "frequency")))
+    #xx <- apply(e, 1, function(i) FUN(data[i,],...))
+    xx <- apply(ind, 1, function(i) FUN(data[(i-width+1):i,],...))
+    if(!is.null(dim(xx))) xx <- t(xx)
+    res <- xts(xx, tt, if (by == 1) attr(data, "frequency"))
   }
-  if( by.column && !is.null(dim(cdata)) ) {
-    colnames(res) <- colnames(cdata)
+  if( na.pad ) {
+    tmp <- merge(res, xts(,idx, attr(data, "frequency")))
+    if(is.null(colnames(res))) {
+      colnames(tmp) <- colnames(res)
+    }
+    res <- tmp
+  }
+  if( by.column && !is.null(dim(data)) ) {
+    colnames(res) <- colnames(data)
   }
   return(res)
 } 
