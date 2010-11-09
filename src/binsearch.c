89e449c7830e87bf96029748ad8dc55ab6bab0b4
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2010-11-09 16:00:26 +0000

    o  faster binsearch.c from indexing pkg
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/pkg/xts@544 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/binsearch.c
+++ src/binsearch.c
@@ -4,6 +4,12 @@
 
 SEXP binsearch (SEXP key, SEXP vec, SEXP start)
 { 
+  int *int_vec;
+  int *int_key;
+  double *real_vec;
+  double *real_key;
+
+  int len_vec = length(vec);
   int found = 0;
   int rec = -1;
   int mid;
@@ -19,16 +25,19 @@ SEXP binsearch (SEXP key, SEXP vec, SEXP start)
   
   if(TYPEOF(vec) == INTSXP) {
   PROTECT(key = coerceVector(key, INTSXP));
+  int_vec = INTEGER(vec);
+  int_key = INTEGER(key);
+
   while(hi >= lo) {
     mid = (int)((lo+hi)/2);
     if(mid == 0)
       return ScalarInteger(NA_INTEGER);
     //if(mid != 0 && INTEGER(key)[0] < INTEGER(vec)[mid]) {
-    if(mid != 0 && INTEGER(key)[0] < INTEGER(vec)[mid-1]) {
+    if(mid != 0 && int_key[0] < int_vec[mid-1]) {
       hi = mid-1;
     } 
     //else if(mid != 0 && INTEGER(key)[0] > INTEGER(vec)[mid]) {
-    else if(mid != 0 && INTEGER(key)[0] > INTEGER(vec)[mid-1]) {
+    else if(mid != 0 && int_key[0] > int_vec[mid-1]) {
       lo = mid+1;
     }
     else {
@@ -37,14 +46,14 @@ SEXP binsearch (SEXP key, SEXP vec, SEXP start)
         if(!LOGICAL(start)[0]) {
           while(1) {
             //if(mid == length(vec) || INTEGER(vec)[mid+1] != INTEGER(key)[0])
-            if(mid == length(vec) || INTEGER(vec)[mid] != INTEGER(key)[0])
+            if(mid == len_vec || int_vec[mid] != int_key[0])
               break;
             mid++;
           }
         } else {
           while(1) {
             //if(mid == 1 || INTEGER(vec)[mid-1] != INTEGER(key)[0])
-            if(mid == 1 || INTEGER(vec)[mid-2] != INTEGER(key)[0])
+            if(mid == 1 || int_vec[mid-2] != int_key[0])
               break;
             mid--;
           }
@@ -74,16 +83,19 @@ SEXP binsearch (SEXP key, SEXP vec, SEXP start)
   } else 
   if(TYPEOF(vec) == REALSXP) {
   PROTECT(key = coerceVector(key,REALSXP));
+  real_vec = REAL(vec);
+  real_key = REAL(key);
+
   while(hi >= lo) {
     mid = (int)((lo+hi)/2);
     if(mid == 0)
       return ScalarInteger(NA_INTEGER);
     //if(mid != 0 && REAL(key)[0] < REAL(vec)[mid]) {
-    if(mid != 0 && REAL(key)[0] < REAL(vec)[mid-1]) {
+    if(mid != 0 && real_key[0] < real_vec[mid-1]) {
       hi = mid-1;
     } 
     //else if(mid != 0 && REAL(key)[0] > REAL(vec)[mid]) {
-    else if(mid != 0 && REAL(key)[0] > REAL(vec)[mid-1]) {
+    else if(mid != 0 && real_key[0] > real_vec[mid-1]) {
       lo = mid+1;
     }
     else {
@@ -92,14 +104,14 @@ SEXP binsearch (SEXP key, SEXP vec, SEXP start)
         if(!LOGICAL(start)[0]) {
           while(1) {
             //if(mid == length(vec) || REAL(vec)[mid+1] != REAL(key)[0])
-            if(mid == length(vec) || REAL(vec)[mid] != REAL(key)[0])
+            if(mid == len_vec || real_vec[mid] != real_key[0])
               break;
             mid++;
           }
         } else {
           while(1) {
             //if(mid == 1 || REAL(vec)[mid-1] != REAL(key)[0])
-            if(mid == 1 || REAL(vec)[mid-2] != REAL(key)[0])
+            if(mid == 1 || real_vec[mid-2] != real_key[0])
               break;
             mid--;
           }
@@ -125,5 +137,6 @@ SEXP binsearch (SEXP key, SEXP vec, SEXP start)
    UNPROTECT(1); return ScalarInteger(rec);
   }
   }
+  return R_NilValue; /* satisfy the compiler */
 }
 
