ca64beae7adb4b43b9b68dbd40db03cc070adc27
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2011-01-21 21:29:48 +0000

    o  further cleaned up code
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/pkg/xts@563 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/unique.time.c
+++ src/unique.time.c
@@ -5,11 +5,11 @@
 SEXP make_unique (SEXP index, SEXP eps_) {
   SEXP newindex;
   int P=0, len, i;
-  double eps = asReal(eps_);
   len=length(index);
-  
-/*  int  *index_int, *newindex_int; */
-  double  *index_real, *newindex_real;
+
+  double eps = asReal(eps_);
+  double *index_real,
+         *newindex_real;
 
   if( TYPEOF(index) == INTSXP) {
     PROTECT(index = coerceVector(index,REALSXP)); P++;
@@ -17,38 +17,19 @@ SEXP make_unique (SEXP index, SEXP eps_) {
   
   PROTECT(newindex = allocVector(REALSXP, length(index))); P++;
   copyAttributes(index, newindex);
+  index_real = REAL(index);
+  newindex_real = REAL(newindex);
 
-/*
-  switch( TYPEOF(index)) {
-    case INTSXP:
-      index_int = INTEGER(index);
-      newindex_int = INTEGER(newindex);
-      newindex_int[0] = index_int[0];
-      for(i=1; i<len; i++) {
-        if(index_int[i-1] == index_int[i])
-          newindex_int[i] = newindex_int[i-1] + eps;
-        else
-          newindex_int[i] = index_int[i];
-      }
-      break;
-    case REALSXP:
-*/
-      index_real = REAL(index);
-      newindex_real = REAL(newindex);
-      newindex_real[0] = index_real[0]; 
-      for(i=1; i<len; i++) {
-        if(index_real[i-1] == index_real[i])
-          newindex_real[i] = newindex_real[i-1] + eps;
-        else
-          newindex_real[i] = index_real[i];
-      }
-/*
-      break;
+  newindex_real[0] = index_real[0]; 
+  for(i=1; i<len; i++) {
+    if(index_real[i-1] == index_real[i])
+      newindex_real[i] = newindex_real[i-1] + eps;
+    else
+      newindex_real[i] = index_real[i];
   }
-*/
+
   UNPROTECT(P);
   return(newindex);
-
 }
 
 SEXP make_index_unique (SEXP x_, SEXP eps_) {
@@ -57,52 +38,5 @@ SEXP make_index_unique (SEXP x_, SEXP eps_) {
   copyAttributes(x_, result); /* copy all attrib except index, and dim-related */
   setAttrib(result, install("index"), make_unique(getAttrib(x_, install("index")), eps_));
   UNPROTECT(1);
-  return(result);
-}
-
-SEXP make_time_unique ( SEXP x_, SEXP eps_) {
-  SEXP result, index, newindex;
-  int P=0, nrs, i;
-  double eps = asReal(eps_);
-  nrs=nrows(x_);
-  
-  int *result_int, *index_int, *newindex_int;
-  double *result_real, *index_real, *newindex_real;
-  
-  PROTECT(result = coredata_xts(x_)); P++;
-
-  copyAttributes(x_, result); /* copy all attrib except index, and dim-related */
-
-  index = getAttrib(x_, install("index"));
-  PROTECT(newindex = allocVector(TYPEOF(index), length(index))); P++;
-  copyAttributes(index, newindex);
-
-  switch( TYPEOF(index)) {
-    case INTSXP:
-      /* this is probably not needed - maybe we force coercion to double first */
-      index_int = INTEGER(index);
-      newindex_int = INTEGER(newindex);
-      newindex_int[0] = index_int[0];
-      for(i=1; i<nrs; i++) {
-        if(index_int[i-1] == index_int[i])
-          newindex_int[i] = newindex_int[i-1] + eps;
-        else
-          newindex_int[i] = index_int[i];
-      }
-      break;
-    case REALSXP:
-      index_real = REAL(index);
-      newindex_real = REAL(newindex);
-      newindex_real[0] = index_real[0]; 
-      for(i=1; i<nrs; i++) {
-        if(index_real[i-1] == index_real[i])
-          newindex_real[i] = newindex_real[i-1] + eps;
-        else
-          newindex_real[i] = index_real[i];
-      }
-      break;
-  }
-  setAttrib(result, install("index"), newindex);
-  UNPROTECT(2);
   return(result);
 }
