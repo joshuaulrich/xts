6efeeeadcf23c8dab31fb15df6256381823fa053
Jeffrey A. Ryan <jeff.a.ryan@gmail.com>
2011-08-23 16:43:07 +0000

    o  subsetting zero-width xts objects now behaves like zoo, with NA    values returned for valid row requests. o  updated to 0.8-3
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/pkg/xts@601 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/xts.methods.R
+++ R/xts.methods.R
@@ -34,6 +34,8 @@ function(x, i, j, drop = FALSE, which.i=FALSE,...)
     USE_EXTRACT <- FALSE # initialize to FALSE
     if(is.null(dim(x))) {
       nr <- length(x)
+      if(nr==0) 
+        return( .xts(rep(NA,length(i)), .index(x)[i], .indexCLASS=indexClass(x)) )
       nc <- 1L
     } else {
       nr <- nrow(x)
