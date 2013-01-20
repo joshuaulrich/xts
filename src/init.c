d2fbd5472a5c6271ab432e51869ecf525ec84914
Dirk Eddelbuettel <edd@debian.org>
2013-01-20 15:10:59 +0000

    o  remove commented out earlier registrations
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/pkg/xts@763 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/init.c
+++ src/init.c
@@ -1,9 +1,9 @@
 /*
 #   xts: eXtensible time-series 
 #
-#   Copyright (C) 2008  Jeffrey A. Ryan jeff.a.ryan @ gmail.com
+#   Copyright (C) 2008 - 2013  Jeffrey A. Ryan jeff.a.ryan @ gmail.com
 #
-#   Contributions from Joshua M. Ulrich
+#   Contributions from Joshua M. Ulrich and Dirk Eddelbuettel
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -62,11 +62,9 @@ void R_init_xts(DllInfo *info)
 
   /* used by external packages linking to internal xts code from C */
   R_RegisterCCallable("xts","do_is_ordered",(DL_FUNC) &do_is_ordered);
-  /*R_RegisterCCallable("xts","coredata",     (DL_FUNC) &coredata);*/
   R_RegisterCCallable("xts","coredata_xts", (DL_FUNC) &coredata_xts);
   R_RegisterCCallable("xts","isXts",        (DL_FUNC) &isXts);
   R_RegisterCCallable("xts","tryXts",       (DL_FUNC) &tryXts);
-  /* RegisterXTS(rbindXts); */
   R_RegisterCCallable("xts","do_rbind_xts", (DL_FUNC) &do_rbind_xts);
   R_RegisterCCallable("xts","naCheck",      (DL_FUNC) &naCheck);
   R_RegisterCCallable("xts","lagXts",       (DL_FUNC) &lagXts);
