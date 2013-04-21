dd8c8352d356932abb6bb2ffd83d51b884fce776
Joshua M. Ulrich <josh.m.ulrich@gmail.com>
2013-04-21 16:36:44 +0000

    - re-patch zero-width xts subsetting (bug #2669)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/xts/pkg/xts@771 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/xts.methods.R
+++ R/xts.methods.R
@@ -34,13 +34,8 @@ function(x, i, j, drop = FALSE, which.i=FALSE,...)
     USE_EXTRACT <- FALSE # initialize to FALSE
     if(is.null(dim(x))) {
       nr <- length(x)
-      if(nr==0) {
-        ix <- .index(x)
-        tmp <- .xts(rep(NA,length(ix)), ix)
-        indexClass(tmp) <- indexClass(x)
-        indexTZ(tmp) <- indexTZ(x)
-        return(tmp[i])
-      }
+      if(nr==0)
+        return( xts(rep(NA,length(index(x))), index(x))[i] )
       nc <- 1L
     } else {
       nr <- nrow(x)
