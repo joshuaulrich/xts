63908e7367cfec0ecfa5d9873d034caa626db7aa
Joshua Ulrich <josh.m.ulrich@gmail.com>
2015-05-03 20:27:52 -0500

    Add license header
    And remove PortfolioAnalytics footer.

--- R/modify.args.R
+++ R/modify.args.R
@@ -1,3 +1,22 @@
+#
+#   xts: eXtensible time-series
+#
+#   Copyright (C) 2009-2015  Jeffrey A. Ryan jeff.a.ryan @ gmail.com
+#
+#   Contributions from Ross Bennett and Joshua M. Ulrich
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 modify.args <- function(formals, arglist, ..., dots=FALSE)
 {
@@ -52,16 +71,3 @@ modify.args <- function(formals, arglist, ..., dots=FALSE)
 # .formals$`...` <- NULL
 # 
 # tmp_val <- do.call(indicator$name, .formals)
-
-
-###############################################################################
-# R (http://r-project.org/) Numeric Methods for Optimization of Portfolios
-#
-# Copyright (c) 2004-2014 Brian G. Peterson, Peter Carl, Ross Bennett, Kris Boudt
-#
-# This library is distributed under the terms of the GNU Public License (GPL)
-# for full details see the file COPYING
-#
-# $Id: utils.R 3302 2014-01-19 19:52:42Z braverock $
-#
-###############################################################################
