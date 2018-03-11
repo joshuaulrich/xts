d1e134b033192b74560d6d8b8ad26126bf3e5f6d
Joshua Ulrich <josh.m.ulrich@gmail.com>
2018-03-11 09:01:33 -0500

    Add more coredata.xts() unit tests
    See #223.

--- inst/unitTests/runit.coredata.R
+++ inst/unitTests/runit.coredata.R
@@ -1,3 +1,45 @@
+test.coredata_vector <- function() {
+  x <- xts(1, as.Date("2018-03-02"))
+  z <- as.zoo(x)
+
+  checkIdentical(coredata(x), coredata(z))
+}
+
+test.coredata_named_vector <- function() {
+  x <- xts(c(hello = 1), as.Date("2018-03-02"))
+  z <- as.zoo(x)
+
+  checkIdentical(coredata(x), coredata(z))
+}
+
+test.coredata_matrix <- function() {
+  x <- xts(cbind(1, 9), as.Date("2018-03-02"))
+  z <- as.zoo(x)
+
+  checkIdentical(coredata(x), coredata(z))
+}
+
+test.coredata_named_matrix <- function() {
+  x <- xts(cbind(hello = 1, world = 9), as.Date("2018-03-02"))
+  z <- as.zoo(x)
+
+  checkIdentical(coredata(x), coredata(z))
+}
+
+test.coredata_data.frame <- function() {
+  x <- xts(data.frame(hello = 1, world = 9), as.Date("2018-03-02"))
+  z <- as.zoo(x)
+
+  checkIdentical(coredata(x), coredata(z))
+}
+
+test.coredata_ts <- function() {
+  x <- xts(ts(1), as.Date("2018-03-02"))
+  z <- as.zoo(x)
+
+  checkIdentical(coredata(x), coredata(z))
+}
+
 # empty objects
 test.coredata_empty <- function() {
   x <- xts(, as.Date("2018-03-02"))
