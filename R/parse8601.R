b666e9abfde042de3883f02cdcba1bd9c8689fac
Joshua Ulrich <josh.m.ulrich@gmail.com>
2018-05-03 06:48:38 -0500

    Add missing copyright headers

--- R/parse8601.R
+++ R/parse8601.R
@@ -1,3 +1,24 @@
+#
+#   xts: eXtensible time-series
+#
+#   Copyright (C) 2008  Jeffrey A. Ryan jeff.a.ryan @ gmail.com
+#
+#   Contributions from Joshua M. Ulrich
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # This function corresponds to the ISO 8601 standard
 # for specifying dates and times as described in
 # the ISO 8601:2004e standard.
