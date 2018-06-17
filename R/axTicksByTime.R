4b58afdc744a196922ea5466d8dffcad60c6d53e
Joshua Ulrich <josh.m.ulrich@gmail.com>
2018-06-17 10:25:53 -0500

    Remove whitespace, use curly braces, double-quotes
    Update format only. No functional changes.

--- R/axTicksByTime.R
+++ R/axTicksByTime.R
@@ -19,66 +19,78 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-axTicksByTime <- function(x, ticks.on='auto', k=1, 
-                          labels=TRUE, format.labels=TRUE, ends=TRUE,
-                           gt = 2, lt = 30) {
-    # if a vector of times/dates, convert to dummy xts object
-    if(timeBased(x)) x <- xts(rep(1,length(x)),x)
-    
-    ticks.on <- ticks.on[1L]
-    # special-case for "secs" and "mins"
-    if(ticks.on == "secs" || ticks.on == "mins")
-      ticks.on <- substr(ticks.on, 1L, 3L)
+axTicksByTime <-
+function(x, ticks.on = "auto", k = 1, labels = TRUE, format.labels = TRUE,
+         ends = TRUE, gt = 2, lt = 30)
+{
+  # if a vector of times/dates, convert to dummy xts object
+  if (timeBased(x)) {
+    x <- xts(rep(1, length(x)), x)
+  }
 
-    tick.opts <- c("years", "months", "weeks", "days", "hours", 
-        "minutes", "seconds")
-    ticks.on <- match.arg(ticks.on, c("auto", tick.opts))
+  ticks.on <- ticks.on[1L]
+  # special-case for "secs" and "mins"
+  if (ticks.on == "secs" || ticks.on == "mins") {
+    ticks.on <- substr(ticks.on, 1L, 3L)
+  }
 
-    if (ticks.on == "auto") {
-        tick.k.opts <- c(10, 5, 2, 1, 6, 1, 1, 1, 4, 2, 1, 30, 15, 1, 1)
-        tick.opts <- rep(tick.opts, c(4, 2, 1, 1, 3, 3, 1))
-        is <- structure(rep(0,length(tick.opts)),.Names=tick.opts)
-        for(i in 1:length(tick.opts)) {
-          ep <- endpoints(x, tick.opts[i], tick.k.opts[i])
-          is[i] <- length(ep) -1
-          if(is[i] > lt) 
-            break
-        }   
-        loc <- rev(which(is > gt & is < lt))[1L]
-        cl <- tick.opts[loc]
-        ck <- tick.k.opts[loc]
+  tick.opts <- c("years", "months", "weeks", "days", "hours",
+    "minutes", "seconds")
+  ticks.on <- match.arg(ticks.on, c("auto", tick.opts))
+
+  if (ticks.on == "auto") {
+    tick.k.opts <- c(10, 5, 2, 1, 6, 1, 1, 1, 4, 2, 1, 30, 15, 1, 1)
+    tick.opts <- rep(tick.opts, c(4, 2, 1, 1, 3, 3, 1))
+    is <- structure(rep(0, length(tick.opts)), .Names = tick.opts)
+    for (i in 1:length(tick.opts)) {
+      ep <- endpoints(x, tick.opts[i], tick.k.opts[i])
+      is[i] <- length(ep) - 1
+      if (is[i] > lt) {
+        break
+      }
+    }
+    loc <- rev(which(is > gt & is < lt))[1L]
+    cl <- tick.opts[loc]
+    ck <- tick.k.opts[loc]
+  } else {
+    cl <- ticks.on[1L]
+    ck <- k
+  }
+
+  if (is.null(cl) || is.na(cl) || is.na(ck)) {
+    ep <- c(0, NROW(x))
+  } else {
+    ep <- endpoints(x, cl, ck)
+  }
+  if (ends) {
+    ep <- ep + c(rep(1, length(ep) - 1), 0)
+  }
+
+  if (labels) {
+    if (is.logical(format.labels) || is.character(format.labels)) {
+      # format by platform...
+      unix <- (.Platform$OS.type == "unix")
+      # ...and level of time detail
+      fmt <- switch(periodicity(x)$scale,
+        weekly    = ,
+        daily     = if (unix) "%b %d%n%Y" else "%b %d %Y",
+        minute    = ,
+        hourly    = if (unix) "%b %d%n%H:%M" else "%b %d %H:%M",
+        seconds   = if (unix) "%b %d%n%H:%M:%S" else "%b %d %H:%M:%S",
+                    if (unix) "%n%b%n%Y" else "%b %Y")
+
+      # special case yearqtr index
+      if (inherits(index(x), "yearqtr")) {
+        fmt <- "%Y-Q%q"
+      }
+
+      if (is.character(format.labels)) {
+        fmt <- format.labels
+      }
+      names(ep) <- format(index(x)[ep], fmt)
     } else {
-        cl <- ticks.on[1L]
-        ck <- k
+      names(ep) <- as.character(index(x)[ep])
     }
-
-    if (is.null(cl) || is.na(cl) || is.na(ck)) {
-        ep <- c(0, NROW(x))
-    } else  ep <- endpoints(x, cl, ck) 
-    if(ends)
-      ep <- ep + c(rep(1,length(ep)-1),0)
-    
-
-    if(labels) {
-      if(is.logical(format.labels) || is.character(format.labels)) {
-        # format by platform...
-        unix <- (.Platform$OS.type == "unix")
-        # ...and level of time detail
-        fmt <- switch(periodicity(x)$scale,
-          weekly    = ,
-          daily     = if (unix) '%b %d%n%Y' else '%b %d %Y',
-          minute    = ,
-          hourly    = if (unix) '%b %d%n%H:%M' else '%b %d %H:%M',
-          seconds   = if (unix) '%b %d%n%H:%M:%S' else '%b %d %H:%M:%S',
-                      if (unix) '%n%b%n%Y' else '%b %Y')
-
-        # special case yearqtr index
-        if(inherits(index(x), "yearqtr"))
-          fmt <- '%Y-Q%q'
-
-        if(is.character(format.labels)) fmt <- format.labels
-        names(ep) <- format(index(x)[ep],fmt)
-      } else names(ep) <- as.character(index(x)[ep])
-    }
-    ep
+  }
+  ep
 }
