7d2367bee923a6cef0df1caaef301338db54c645
Joshua Ulrich <josh.m.ulrich@gmail.com>
2018-10-06 09:09:08 -0500

    Add delayed namespace registration for R>=3.6.0
    CRAN requested packages add delayed namespace registration for methods
    whose generics exist in packages in Suggests.
    
    Also add delayed namespace registration for R<3.6.0 by using the
    work-around in register_s3_method() called in .onLoad().

--- R/zzz.R
+++ R/zzz.R
@@ -26,6 +26,32 @@
 # Environment for our xts chart objects (xts_chob)
 .plotxtsEnv <- new.env()
 
+register_s3_method <-
+function(pkg, generic, class, fun = NULL)
+{
+  stopifnot(is.character(pkg), length(pkg) == 1L)
+  stopifnot(is.character(generic), length(generic) == 1L)
+  stopifnot(is.character(class), length(class) == 1L)
+
+  if (is.null(fun)) {
+    fun <- get(paste0(generic, ".", class), envir = parent.frame())
+  } else {
+    stopifnot(is.function(fun))
+  }
+
+  if (isNamespaceLoaded(pkg)) {
+    registerS3method(generic, class, fun, envir = asNamespace(pkg))
+  }
+
+  # Always register hook in case package is later unloaded & reloaded
+  setHook(
+    packageEvent(pkg, "onLoad"),
+    function(...) {
+      registerS3method(generic, class, fun, envir = asNamespace(pkg))
+    }
+  )
+}
+
 .onLoad <- function(libname, pkgname)
 {
 #  if(Sys.getenv("TZ") == "") {
@@ -36,6 +62,11 @@
 #    packageStartupMessage("xts now requires a valid TZ environment variable to be set")
 #    packageStartupMessage(" your current TZ:",paste(Sys.getenv("TZ")))
 #  }
+  if (getRversion() < "3.6.0") {
+    register_s3_method("timeSeries", "as.timeSeries", "xts")
+    register_s3_method("fts", "as.fts", "xts")
+  }
+  invisible()
 }
 
 .onUnload <- function(libpath) {
