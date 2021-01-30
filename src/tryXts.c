38772558b97502d7a6d8e391879b4ca0e92cec22
Joshua Ulrich <josh.m.ulrich@gmail.com>
2021-01-30 09:37:01 -0600

    Make tryXts() C function work if xts not attached
    The tryXts() C function evaluated the try.xts() R function in the
    global environment, which would only work if xts is attached to the
    search path.
    
    Thanks to Kurt Hornik for the report and confirming the patch.

--- src/tryXts.c
+++ src/tryXts.c
@@ -27,12 +27,14 @@
 SEXP tryXts (SEXP x)
 {
   if( !Rf_asInteger(isXts(x)) ) {
-    SEXP s, t, result;
+    SEXP s, t, result, env, str_xts;
     PROTECT(s = t = allocList(2));
     SET_TYPEOF(s, LANGSXP);
     SETCAR(t, install("try.xts")); t = CDR(t);
     SETCAR(t, x); t=CDR(t);
-    PROTECT(result = eval(s, R_GlobalEnv));
+    PROTECT(str_xts = mkString("xts"));
+    PROTECT(env = R_FindNamespace(str_xts));
+    PROTECT(result = eval(s, env));
     if( !Rf_asInteger(isXts(result)) ) {
       UNPROTECT(2);
       error("rbind.xts requires xtsible data");
