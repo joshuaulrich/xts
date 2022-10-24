00e3bc680a7a384285be315137742d4c85cac156
Joshua Ulrich <josh.m.ulrich@gmail.com>
2022-10-24 12:02:27 -0500

    Only show head and tail for long series
    Refactor print.xts() to only show the first and last 'max' rows if the
    number of rows is > 'trunc.rows'. Also truncate the number of columns
    if they would wrap to a new line.
    
    See #321.

--- R/print.R
+++ R/print.R
@@ -18,33 +18,105 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
-`print.xts` <-
-function(x,fmt,...) {
+print.xts <-
+  function(x,
+           fmt,
+           ...,
+           max = NULL,
+           trunc.rows = 40,
+           trunc.cols = NULL)
+{
   check.TZ(x)
-  if(missing(fmt)) 
+
+  if (is.null(max)) {
+    max <- getOption("xts.max.print", 10)
+  }
+
+  if (missing(fmt)) {
     fmt <- tformat(x)
-  if(is.null(fmt))
+  }
+  if (is.null(fmt)) {
     fmt <- TRUE
+  }
+
+  if (!hasArg("quote")) {
+    quote <- FALSE
+  }
+  if (!hasArg("right")) {
+    right <- TRUE
+  }
   
-  xx <- coredata(x, fmt)
-  if(length(xx) == 0) {
-    if(!is.null(dim(x))) {
-      p <- structure(vector(storage.mode(xx)), dim = dim(x),
-                     dimnames = list(format(index(x)),colnames(x)))
+  nr <- NROW(x)
+  nc <- NCOL(x)
+
+  if (!is.null(trunc.cols)) {
+    if (isTRUE(trunc.cols)) {
+      # capture print output until 1 column would wrap
+      capture.printed.cols <-
+        function(i, obj)
+      {
+        o <- utils::capture.output(print.default(obj[, seq_len(i)]))
+        max(nchar(o)) # max characters printed for this number of cols
+      }
+
+      pcols <- sapply(seq_len(nc), capture.printed.cols, obj = x, ...)
+      nc <- which.min(getOption("width") / pcols)
+    } else if (is.numeric(trunc.cols) && is.finite(trunc.cols)) {
+      nc <- trunc.cols
+    }
+  }
+
+  seq.max <- seq_len(max)
+  seq.col <- seq_len(nc)
+
+  if (nr > trunc.rows + 1) {
+    index <- as.character(index(x))
+    index <- c(index[seq.max], "...", index[(nr-max+1):nr])
+    y <- rbind(
+      format(as.matrix(x[seq.max, seq.col])),
+      format(matrix(rep("", nc), nrow = 1)),
+      format(as.matrix(x[(nr-max+1):nr, seq.col]))
+    )
+    rownames(y) <- format(index, justify = "right")
+    colnames(y) <- colnames(x[, seq.col])
+  } else {
+    y <- coredata(x, fmt)
+  }
+
+  if (length(y) == 0) {
+    if (!is.null(dim(x))) {
+      p <- structure(vector(storage.mode(y)), dim = dim(x),
+                     dimnames = list(format(index(x)), colnames(x)))
       print(p)
     } else {
       cat('Data:\n')
-      print(vector(storage.mode(xx)))
+      print(vector(storage.mode(y)))
       cat('\n')
       cat('Index:\n')
       index <- index(x)
-      if(length(index) == 0) {
+      if (length(index) == 0) {
         print(index)
       } else {
-        str(index(x))
+        print(str(index))
       }
     }
-  } else print(xx, ...)
-}
+  } else {
+    # Create column names as right-justified column indexes. They're left-
+    # justified by default, which is different than if there are column names.
+    if (is.null(colnames(y))) {
+      cindex <- utils::capture.output(print(y[1,,drop = FALSE]))[1]
+      cindex <- sub("\\s+$", "", cindex)
+      cindex <- paste0(strsplit(cindex, "]")[[1]], "]")
+      cchar <- nchar(cindex)
+      if (NCOL(y) == 1) {
+        cindex <- cindex[1]
+      }
+      cindex[1] <- substring(cindex[1], cchar[1] - min(cchar), cchar[1])
+      colnames(y) <- cindex
+    }
 
+    print(y, quote = quote, right = right, ...)
+  }
+
+  invisible(x)
+}
