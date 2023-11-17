8d473d151d8155c0bcf05d4aa3a7393751a90746
Joshua Ulrich <josh.m.ulrich@gmail.com>
2023-11-17 13:51:12 -0600

    Reduce tinytest default verbosity

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -2,7 +2,7 @@
 if (requireNamespace("tinytest", quietly = TRUE)) {
     suppressPackageStartupMessages(library("xts"))
     use_color <- as.logical(Sys.getenv("_PKG_TINYTEST_COLOR_", FALSE))
-    verbosity <- as.integer(Sys.getenv("_PKG_TINYTEST_VERBOSE_", 2))
+    verbosity <- as.integer(Sys.getenv("_PKG_TINYTEST_VERBOSE_", 1))
     cat("tinytest colored output:", use_color,
         "\ntinytest verbosity:", verbosity, "\n")
     tinytest::test_package("xts", color = use_color, verbose = verbosity)
