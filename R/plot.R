b248b5784d3af66e68f5a74199d77c1e8b59e98b
Ethan Smith <24379655+ethanbsmith@users.noreply.github.com>
2024-11-01 15:31:31 -0600

    isolated par replication to multipanel sc3narios

--- R/plot.R
+++ R/plot.R
@@ -362,15 +362,18 @@ plot.xts <- function(x,
                      legend.loc=NULL,
                      extend.xaxis=FALSE){
 
-  # check for colorset or col argument
-  if(hasArg("colorset")) {
-    col <- eval.parent(plot.call$colorset)
+  if (is.numeric(multi.panel) || (multi.panel == TRUE)){
+    #only for multipanel scenarios. single panel supports colors for each data ppoint in the series
+    # check for colorset or col argument
+    if(hasArg("colorset")) {
+      col <- eval.parent(plot.call$colorset)
+    }
+    # ensure pars have ncol(x) elements
+    col <- rep(col, length.out = NCOL(x))
+    lty <- rep(lty, length.out = NCOL(x))
+    lwd <- rep(lwd, length.out = NCOL(x))
   }
-  # ensure pars have ncol(x) elements
-  col <- rep(col, length.out = NCOL(x))
-  lty <- rep(lty, length.out = NCOL(x))
-  lwd <- rep(lwd, length.out = NCOL(x))
-
+  
   # Small multiples with multiple pages behavior occurs when multi.panel is
   # an integer. (i.e. multi.panel=2 means to iterate over the data in a step
   # size of 2 and plot 2 panels on each page
